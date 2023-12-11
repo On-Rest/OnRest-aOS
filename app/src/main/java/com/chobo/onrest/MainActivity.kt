992e1e3c722a47ce111324f0481f949176b3233e
youloveli <mymhj122@gmail.com>
2023-12-11 10:08:49 +0900

    :art: Adapter kt code publishing

--- app/src/main/java/com/chobo/onrest/MainActivity.kt
+++ app/src/main/java/com/chobo/onrest/MainActivity.kt
@@ -1,21 +1,17 @@
 package com.chobo.onrest
 
 import android.os.Bundle
-import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.chobo.onrest.databinding.ActivityMainBinding
-import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
 // MainActivity.java
 class MainActivity : AppCompatActivity() {
-    private var binding: ActivityMainBinding? = null
-    var missionList = arrayListOf<Mission>(Mission (12,"미션","미션디테일",true))
+    private lateinit var binding : ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val missionAdapter = MainListAdapter(this, missionList)
-        mainListView.adapter = missionAdapter
+        setContentView(R.layout.community)
+        
     }
 }
--- app/src/main/java/com/chobo/onrest/Mission.kt
+++ app/src/main/java/com/chobo/onrest/Mission.kt
@@ -1,4 +1,8 @@
 package com.chobo.onrest
 
-data class Mission(val date: Int, val mission: String, val missionDetail: String,val check: Boolean) {
-}
+data class Mission(
+    val date: Int,
+    val mission: String,
+    val missionDetail: String,
+    val check: Boolean
+)
