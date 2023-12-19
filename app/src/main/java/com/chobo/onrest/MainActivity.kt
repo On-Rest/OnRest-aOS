051b8b44598398a932e9f255b21d646d11bb154b
Github <s23065@gsm.hs.kr>
2023-12-19 18:06:11 +0900

    google login fail

--- app/src/main/java/com/chobo/onrest/MainActivity.kt
+++ app/src/main/java/com/chobo/onrest/MainActivity.kt
@@ -2,6 +2,8 @@ package com.chobo.onrest
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.chobo.onrest.databinding.ActivityMainBinding
 import com.chobo.onrest.databinding.CalendarBinding
@@ -12,11 +14,17 @@ import com.chobo.onrest.databinding.EmotionChoice2HappyBinding
 import com.chobo.onrest.databinding.HeaderBinding
 import com.chobo.onrest.databinding.YourEmotionBinding
 import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.Api
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
 
 // MainActivity.java
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var mGoogleSingInClient: GoogleSignInClient
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -27,7 +35,34 @@ class MainActivity : AppCompatActivity() {
             .requestEmail()
             .build()
 
-        val mGoogleSingInClient = this?.let { GoogleSignIn.getClient(it, gso) }!!
+        mGoogleSingInClient = GoogleSignIn.getClient(this, gso)
+
+        binding.googlebutton.setOnClickListener() {
+            SignIn()
+        }
+    }
+
+    private fun SignIn() {
+        val signInIntent = mGoogleSingInClient.signInIntent
+        resultLauncher.launch(signInIntent)
+    }
+
+    private var resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == 1) {
+            val data: Intent? = result.data
+            val task: Task<GoogleSignInAccount> = GoogleSignIn.getSignedInAccountFromIntent(data)
+            handleSignInResult(task)
+        }
+    }
+
+    private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
+        try {
+            val account = completedTask.getResult(ApiException::class.java)
+            val email = account?.email.toString()
+            val familyName = account?.familyName.toString()
+        } catch(e: ApiException) {
+            Log.w("failed", "signInResult:failed code = " + e.statusCode)
+        }
     }
 
     class Calender : AppCompatActivity() {
