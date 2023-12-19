e4f868f894495f050e3935b76cb2606e02f2b2d3
youloveli <mymhj122@gmail.com>
2023-12-19 16:15:04 +0900

    :bug: 버그수정후 대부분의 이동 완성

--- app/src/main/java/com/chobo/onrest/MainActivity.kt
+++ app/src/main/java/com/chobo/onrest/MainActivity.kt
@@ -10,6 +10,10 @@ import com.chobo.onrest.databinding.EmotionChoice1Binding
 import com.chobo.onrest.databinding.EmotionChoice2AngryBinding
 import com.chobo.onrest.databinding.EmotionChoice2HappyBinding
 import com.chobo.onrest.databinding.HeaderBinding
+import com.chobo.onrest.databinding.PostWriteBinding
+import com.chobo.onrest.databinding.QuestChoiceBinding
+import com.chobo.onrest.databinding.QuestHistoryBinding
+import com.chobo.onrest.databinding.QuestListBinding
 import com.chobo.onrest.databinding.YourEmotionBinding
 
 // MainActivity.java
@@ -18,13 +22,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        val view = binding!!.root
+        val view = binding.root
         setContentView(view)
 
         binding.login.setOnClickListener() {
             startActivity(Intent(this, NaviActivity::class.java))
-            finish()
             overridePendingTransition( android.R.anim.fade_in, android.R.anim.fade_out )
+            finish()
         }
     }
 }
@@ -34,58 +38,54 @@ class Calender : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = CalendarBinding.inflate(layoutInflater)
-        val view = binding!!.root
+        val view = binding.root
         setContentView(view)
 
         binding.speechbubble.setOnClickListener() {
-            startActivity(Intent(this, QuestList::class.java))
-            finish()
+            startActivity(Intent(this, DiaryWrite::class.java))
             overridePendingTransition( android.R.anim.fade_in, android.R.anim.fade_out )
+            finish()
         }
         binding.stripe.setOnClickListener() {
-            startActivity(Intent(this, QuestList::class.java))
-            finish()
+            startActivity(Intent(this, DiaryWrite::class.java))
             overridePendingTransition( android.R.anim.fade_in, android.R.anim.fade_out )
+            finish()
         }
     }
 }
-
 class DiaryWrite : AppCompatActivity() {
     private lateinit var binding: DiaryWriteBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DiaryWriteBinding.inflate(layoutInflater)
-        val view = binding!!.root
+        val view = binding.root
         setContentView(view)
 
         binding.write.setOnClickListener() {
-            val intent = Intent(this, EmotionChoice1::class.java)
+            val intent = Intent(this, YourEmotion::class.java)
             startActivity(intent)
-            finish()
             overridePendingTransition( android.R.anim.fade_in, android.R.anim.fade_out )
+            finish()
         }
     }
 }
-
 class YourEmotion : AppCompatActivity() {
     private lateinit var binding: YourEmotionBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = YourEmotionBinding.inflate(layoutInflater)
-        val view = binding!!.root
+        val view = binding.root
         setContentView(view)
 
         binding.choice1.setOnClickListener() {
-            val intent = Intent(this, EmotionChoice1::class.java)
-            startActivity(intent)
-            finish()
+            startActivity(Intent(this, QuestList::class.java))
             overridePendingTransition( android.R.anim.fade_in, android.R.anim.fade_out )
+            finish()
         }
         binding.choice2.setOnClickListener() {
-            val intent = Intent(this, EmotionChoice1::class.java)
-            startActivity(intent)
-            finish()
+            startActivity(Intent(this, EmotionChoice1::class.java))
             overridePendingTransition( android.R.anim.fade_in, android.R.anim.fade_out )
+            finish()
         }
     }
 }
@@ -94,26 +94,23 @@ class EmotionChoice1 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = EmotionChoice1Binding.inflate(layoutInflater)
-        val view = binding!!.root
+        val view = binding.root
         setContentView(view)
 
         binding.happyface.setOnClickListener() {
-            val intent = Intent(this, EmotionChoice2Happy::class.java)
-            startActivity(intent)
-            finish()
+            startActivity(Intent(this, EmotionChoice2Happy::class.java))
             overridePendingTransition( android.R.anim.fade_in, android.R.anim.fade_out )
+            finish()
         }
         binding.angryface.setOnClickListener() {
-            val intent = Intent(this, EmotionChoice2Angry::class.java)
-            startActivity(intent)
-            finish()
+            startActivity(Intent(this, EmotionChoice2Angry::class.java))
             overridePendingTransition( android.R.anim.fade_in, android.R.anim.fade_out )
+            finish()
         }
         binding.sadface.setOnClickListener() {
-            val intent = Intent(this, EmotionChoice2Sad::class.java)
-            startActivity(intent)
-            finish()
+            startActivity(Intent(this, EmotionChoice2Sad::class.java))
             overridePendingTransition( android.R.anim.fade_in, android.R.anim.fade_out )
+            finish()
         }
     }
 }
@@ -122,26 +119,23 @@ class EmotionChoice2Happy : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = EmotionChoice2HappyBinding.inflate(layoutInflater)
-        val view = binding!!.root
+        val view = binding.root
         setContentView(view)
 
         binding.choice1.setOnClickListener() {
-            val intent = Intent(this, QuestList::class.java)
-            startActivity(intent)
-            finish()
+            startActivity(Intent(this, QuestChoice::class.java))
             overridePendingTransition( android.R.anim.fade_in, android.R.anim.fade_out )
+            finish()
         }
         binding.choice2.setOnClickListener() {
-            val intent = Intent(this, QuestList::class.java)
-            startActivity(intent)
-            finish()
+            startActivity(Intent(this, QuestChoice::class.java))
             overridePendingTransition( android.R.anim.fade_in, android.R.anim.fade_out )
+            finish()
         }
         binding.choice3.setOnClickListener() {
-            val intent = Intent(this, QuestList::class.java)
-            startActivity(intent)
-            finish()
+            startActivity(Intent(this, QuestChoice::class.java))
             overridePendingTransition( android.R.anim.fade_in, android.R.anim.fade_out )
+            finish()
         }
     }
 }
@@ -150,26 +144,23 @@ class EmotionChoice2Angry : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = EmotionChoice2AngryBinding.inflate(layoutInflater)
-        val view = binding!!.root
+        val view = binding.root
         setContentView(view)
 
         binding.choice1.setOnClickListener() {
-            val intent = Intent(this, QuestList::class.java)
-            startActivity(intent)
-            finish()
+            startActivity(Intent(this, QuestChoice::class.java))
             overridePendingTransition( android.R.anim.fade_in, android.R.anim.fade_out )
+            finish()
         }
         binding.choice2.setOnClickListener() {
-            val intent = Intent(this, QuestList::class.java)
-            startActivity(intent)
-            finish()
+            startActivity(Intent(this, QuestChoice::class.java))
             overridePendingTransition( android.R.anim.fade_in, android.R.anim.fade_out )
+            finish()
         }
         binding.choice3.setOnClickListener() {
-            val intent = Intent(this, QuestList::class.java)
-            startActivity(intent)
-            finish()
+            startActivity(Intent(this, QuestChoice::class.java))
             overridePendingTransition( android.R.anim.fade_in, android.R.anim.fade_out )
+            finish()
         }
     }
 }
@@ -178,26 +169,23 @@ class EmotionChoice2Sad : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = EmotionChoice2HappyBinding.inflate(layoutInflater)
-        val view = binding!!.root
+        val view = binding.root
         setContentView(view)
 
         binding.choice1.setOnClickListener() {
-            val intent = Intent(this, QuestList::class.java)
-            startActivity(intent)
-            finish()
+            startActivity(Intent(this, QuestChoice::class.java))
             overridePendingTransition( android.R.anim.fade_in, android.R.anim.fade_out )
+            finish()
         }
         binding.choice2.setOnClickListener() {
-            val intent = Intent(this, QuestList::class.java)
-            startActivity(intent)
-            finish()
+            startActivity(Intent(this, QuestChoice::class.java))
             overridePendingTransition( android.R.anim.fade_in, android.R.anim.fade_out )
+            finish()
         }
         binding.choice3.setOnClickListener() {
-            val intent = Intent(this, QuestList::class.java)
-            startActivity(intent)
-            finish()
+            startActivity(Intent(this, QuestChoice::class.java))
             overridePendingTransition( android.R.anim.fade_in, android.R.anim.fade_out )
+            finish()
         }
     }
 }
@@ -206,8 +194,14 @@ class QuestList : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = EmotionChoice2HappyBinding.inflate(layoutInflater)
-        val view = binding!!.root
+        val view = binding.root
         setContentView(view)
+
+        binding.speechbubble.setOnClickListener() {
+            startActivity(Intent(this, NaviActivity::class.java))
+            overridePendingTransition( android.R.anim.fade_in, android.R.anim.fade_out )
+            finish()
+        }
     }
 }
 class Header : AppCompatActivity() {
@@ -215,13 +209,68 @@ class Header : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = HeaderBinding.inflate(layoutInflater)
-        val view = binding!!.root
+        val view = binding.root
         setContentView(view)
 
         binding.wholerest.setOnClickListener() {
-            startActivity(Intent(this, Calender::class.java))
-            finish()
+            startActivity(Intent(this, NaviActivity::class.java))
             overridePendingTransition( android.R.anim.fade_in, android.R.anim.fade_out )
+            finish()
+        }
+    }
+}
+class QuestChoice : AppCompatActivity() {
+    private lateinit var binding: QuestChoiceBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = QuestChoiceBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.choice1.setOnClickListener() {
+            startActivity(Intent(this, QuestHistory::class.java))
+            overridePendingTransition( android.R.anim.fade_in, android.R.anim.fade_out )
+            finish()
+        }
+        binding.choice2.setOnClickListener() {
+            startActivity(Intent(this, QuestHistory::class.java))
+            overridePendingTransition( android.R.anim.fade_in, android.R.anim.fade_out )
+            finish()
+        }
+        binding.choice3.setOnClickListener() {
+            startActivity(Intent(this, NaviActivity::class.java))
+            overridePendingTransition( android.R.anim.fade_in, android.R.anim.fade_out )
+            finish()
+        }
+    }
+}
+class QuestHistory : AppCompatActivity() {
+    private lateinit var binding: QuestHistoryBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = QuestHistoryBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.idDate.setOnClickListener() {
+            startActivity(Intent(this, QuestList::class.java))
+            overridePendingTransition( android.R.anim.fade_in, android.R.anim.fade_out )
+            finish()
+        }
+    }
+}
+class PostWrite : AppCompatActivity() {
+    private lateinit var binding: PostWriteBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = PostWriteBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.send.setOnClickListener() {
+            startActivity(Intent(this, QuestList::class.java))
+            overridePendingTransition( android.R.anim.fade_in, android.R.anim.fade_out )
+            finish()
         }
     }
 }
