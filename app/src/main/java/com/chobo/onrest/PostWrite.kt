06395dc1e28ec7e442b60b59519d776b1fc8fb96
youloveli <mymhj122@gmail.com>
2023-12-20 16:20:12 +0900

    :sparkles: post_write.xml 태그수 2개 재한기능 추가

--- app/src/main/java/com/chobo/onrest/PostWrite.kt
+++ app/src/main/java/com/chobo/onrest/PostWrite.kt
@@ -5,10 +5,13 @@ import android.graphics.Color
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.widget.CompoundButton
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.ToggleButton
 import androidx.appcompat.app.AppCompatActivity
 import com.chobo.onrest.databinding.PostWriteBinding
+
 
 class PostWrite : AppCompatActivity() {
     private lateinit var binding: PostWriteBinding
@@ -18,12 +21,49 @@ class PostWrite : AppCompatActivity() {
     private lateinit var captionTextView1: TextView
     private val maxCharacters = 300 // 최대 글자 수
     private val maxCharacters1 = 30 // 최대 글자 수
+    var maxToggleCount = 2 // 최대 토글 횟수
+    var currentToggleCount = 0 // 현재 토글 횟수
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = PostWriteBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        var toggleButton1: ToggleButton = findViewById(R.id.sad)
+        var toggleButton2: ToggleButton = findViewById(R.id.helpless)
+        var toggleButton3: ToggleButton = findViewById(R.id.shy)
+        var toggleButton4: ToggleButton = findViewById(R.id.anoying)
+        var toggleButton5: ToggleButton = findViewById(R.id.angry)
+        var toggleButton6: ToggleButton = findViewById(R.id.joyful)
+        var toggleButton7: ToggleButton = findViewById(R.id.tranquility)
+        var toggleButton8: ToggleButton = findViewById(R.id.excited)
+        var toggleButton9: ToggleButton = findViewById(R.id.happy)
+
+        val toggleListener = CompoundButton.OnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked) {
+                if (currentToggleCount >= maxToggleCount) {
+                    // 최대 토글 횟수를 초과하면 다시 체크를 해제합니다.
+                    buttonView.isChecked = false
+                } else {
+                    // 현재 토글 횟수가 최대 토글 횟수보다 작을 경우에만 토글 횟수 증가
+                    currentToggleCount++
+                }
+            } else {
+                // 토글 해제 시 토글 횟수 감소
+                currentToggleCount--
+            }
+        }
+
+        toggleButton1.setOnCheckedChangeListener(toggleListener)
+        toggleButton2.setOnCheckedChangeListener(toggleListener)
+        toggleButton3.setOnCheckedChangeListener(toggleListener)
+        toggleButton4.setOnCheckedChangeListener(toggleListener)
+        toggleButton5.setOnCheckedChangeListener(toggleListener)
+        toggleButton6.setOnCheckedChangeListener(toggleListener)
+        toggleButton8.setOnCheckedChangeListener(toggleListener)
+        toggleButton9.setOnCheckedChangeListener(toggleListener)
 
         editText = binding.detailinput // EditText 참조 가져오기
         captionTextView = binding.detailTextnum // TextView 참조 가져오기
@@ -45,7 +85,7 @@ class PostWrite : AppCompatActivity() {
                     captionTextView.setTextColor(Color.parseColor("#E92626")) // 텍스트 색상을 변경합니다
                 } else {
                     // 글자 수 제한 안에 있을 때 텍스트 색상을 리셋합니다
-                    captionTextView1.setTextColor(Color.parseColor("#89857C")) // 원하는 기본 색상으로 변경하세요
+                    captionTextView1.setTextColor(Color.parseColor("#89857C"))
                 }
             }
 
@@ -74,7 +114,7 @@ class PostWrite : AppCompatActivity() {
                     captionTextView1.setTextColor(Color.parseColor("#E92626")) // 텍스트 색상을 변경합니다
                 } else {
                     // 글자 수 제한 안에 있을 때 텍스트 색상을 리셋합니다
-                    captionTextView1.setTextColor(Color.parseColor("#89857C")) // 원하는 기본 색상으로 변경하세요
+                    captionTextView1.setTextColor(Color.parseColor("#89857C"))
                 }
             }
 
