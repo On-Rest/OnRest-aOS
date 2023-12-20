249bb430574be125f3fdcc6bef90246b694e9b20
youloveli <mymhj122@gmail.com>
2023-12-20 12:33:29 +0900

    :sparkles: comunity_list_view.xml 의 detail을 누르면 post_detail.xml로 넘어가는 기능추가

---
+++ app/src/main/java/com/chobo/onrest/ComunityListView.kt
@@ -0,0 +1,23 @@
+package com.chobo.onrest
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.chobo.onrest.databinding.ComunityListViewBinding
+
+
+class ComunityListView : AppCompatActivity() {
+    private lateinit var binding: ComunityListViewBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ComunityListViewBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.detail.setOnClickListener() {
+            startActivity(Intent(this, PostDetail::class.java))
+            overridePendingTransition( android.R.anim.fade_in, android.R.anim.fade_out )
+            finish()
+        }
+    }
+}
---
+++ app/src/main/java/com/chobo/onrest/PostDetail.kt
@@ -0,0 +1,16 @@
+package com.chobo.onrest
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.chobo.onrest.databinding.PostDetailBinding
+
+
+class PostDetail : AppCompatActivity() {
+    private lateinit var binding: PostDetailBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = PostDetailBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }
+}
