64f2dba9d987360e29f046e35aa52a0840261100
youloveli <mymhj122@gmail.com>
2023-12-20 13:23:41 +0900

    :sparkles: PostDetail 뒤로가기버튼 구현, 하트 그림자 삭제

--- app/src/main/java/com/chobo/onrest/PostDetail.kt
+++ app/src/main/java/com/chobo/onrest/PostDetail.kt
@@ -1,5 +1,6 @@
 package com.chobo.onrest
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.chobo.onrest.databinding.PostDetailBinding
@@ -12,5 +13,9 @@ class PostDetail : AppCompatActivity() {
         binding = PostDetailBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        binding.goback.setOnClickListener() {
+            super.onBackPressed() // 기본 뒤로가기 동작 유지
+        }
     }
 }
