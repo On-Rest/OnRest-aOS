6c1003c019dc44ac9c532634889189d69d21f1ed
Github <s23065@gsm.hs.kr>
2023-12-22 08:05:03 +0900

    [#77] :: 통신전 마지막 파일 정리

--- app/src/main/java/com/chobo/onrest/CommunityFragment.kt
+++ app/src/main/java/com/chobo/onrest/CommunityFragment.kt
@@ -7,23 +7,47 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.chobo.onrest.databinding.CommunityBinding
+import com.chobo.onrest.databinding.ComunityNotFoundBinding
 
 class CommunityFragment : Fragment() {
 
     private lateinit var binding: CommunityBinding
     lateinit var communityAdapter: CommunityAdapter
     val datas = mutableListOf<CommunityData>()
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = CommunityBinding.inflate(inflater, container, false)
-        initRecycler()
+        initRecycler()`
         return binding.root
 
     }
+
+    private fun initRecycler() {
+        communityAdapter = CommunityAdapter(requireContext())
+        binding.list.adapter = communityAdapter
+
+        datas.apply {
+
+            add(CommunityData("오늘의 화난썰", "오늘은 아이디어 페스티벌 프로젝트를 진행했다.", "#화난", "#짜증", 19, 27))
+            add(CommunityData("오늘의 화난썰", "오늘은 아이디어 페스티벌 프로젝트를 진행했다.", "#화난", "#짜증", 19, 27))
+            add(CommunityData("오늘의 화난썰", "오늘은 아이디어 페스티벌 프로젝트를 진행했다.", "#화난", "#짜증", 19, 27))
+            add(CommunityData("오늘의 화난썰", "오늘은 아이디어 페스티벌 프로젝트를 진행했다.", "#화난", "#짜증", 19, 27))
+            add(CommunityData("오늘의 화난썰", "오늘은 아이디어 페스티벌 프로젝트를 진행했다.", "#화난", "#짜증", 19, 27))
+            add(CommunityData("오늘의 화난썰", "오늘은 아이디어 페스티벌 프로젝트를 진행했다.", "#화난", "#짜증", 19, 27))
+            add(CommunityData("오늘의 화난썰", "오늘은 아이디어 페스티벌 프로젝트를 진행했다.", "#화난", "#짜증", 19, 27))
+            add(CommunityData("오늘의 화난썰", "오늘은 아이디어 페스티벌 프로젝트를 진행했다.", "#화난", "#짜증", 19, 27))
+            add(CommunityData("오늘의 화난썰", "오늘은 아이디어 페스티벌 프로젝트를 진행했다.", "#화난", "#짜증", 19, 27))
+            add(CommunityData("오늘의 화난썰", "오늘은 아이디어 페스티벌 프로젝트를 진행했다.", "#화난", "#짜증", 19, 27))
+            add(CommunityData("오늘의 화난썰", "오늘은 아이디어 페스티벌 프로젝트를 진행했다.", "#화난", "#짜증", 19, 27))
+            // 임시 저장 임의 데이터
+        }
+        communityAdapter.datas = datas
+        communityAdapter.notifyDataSetChanged()
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -39,36 +63,5 @@ class CommunityFragment : Fragment() {
         val intent = Intent(requireContext(), clazz)
         startActivity(intent)
         requireActivity().overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
-    }
-    private fun initRecycler() {
-        communityAdapter = CommunityAdapter(requireContext())
-        binding.list.adapter = communityAdapter
-
-        datas.apply {
-            add(CommunityData("햄스터?", "햄스터란...", "#햄", "#찌", 19, 27))
-            add(CommunityData("햄스터?", "햄스터란아니 명훈아 좀더 긴걸 넣었어애지...", "#햄", "#찌", 19, 27))
-            add(CommunityData("햄스터이것도 얼마니긴지알아야지?햄스터이것도 얼마니긴지알아야지?햄스터이것도 얼마니긴지알아야지?", "햄스터란...", "#햄", "#찌", 19, 27))
-            add(CommunityData("햄스터?", "햄스터란...", "#햄", "#찌", 19, 27))
-            add(CommunityData("햄스터?", "햄스터란...", "#햄", "#찌", 19, 100))
-            add(CommunityData("햄스터?", "햄스터란...", "#햄", "#찌", 100, 27))
-            add(CommunityData("햄스터?", "햄스터란...", "#햄", "#찌", 100, 27))
-            add(CommunityData("햄스터?", "햄스터란...", "#햄", "#찌", 100, 27))
-            add(CommunityData("햄스터?", "햄스터란...", "#햄", "#찌", 100, 27))
-            add(CommunityData("햄스터?", "햄스터란...", "#햄이", "#찌", 100, 27))
-            add(CommunityData("햄스터?", "햄스터란", "#햄이야", "#찌", 100000, 27))
-            add(CommunityData("햄스터?", "햄스터란...", "#햄끄러운", "#찌", 100, 27))
-            add(CommunityData("햄스터?", "햄스터란...", "#햄사사롭다", "#찌", 100, 27))
-            add(CommunityData("햄스터?", "햄스터란...", "#햄", "#찌", 100, 27))
-            add(CommunityData("햄스터?", "햄스터란...", "#햄", "#찌", 100, 27))
-            add(CommunityData("햄스터?", "햄스터란...", "#햄", "#찌", 100, 27))
-            add(CommunityData("햄스터?", "햄스터란...", "#햄", "#찌", 100, 27))
-            add(CommunityData("햄스터?", "햄스터란...", "#햄", "#찌", 100, 27))
-            add(CommunityData("햄스터?", "햄스터란...", "#햄", "#찌", 100, 27))
-            add(CommunityData("햄스터?", "햄스터란...", "#햄", "#찌", 100, 27))
-            add(CommunityData("햄스터?", "햄스터란...", "#햄", "#찌", 100, 27))
-            // 임시 지정(수동)
-            communityAdapter.datas = datas
-            communityAdapter.notifyDataSetChanged()
-        }
     }
 }
