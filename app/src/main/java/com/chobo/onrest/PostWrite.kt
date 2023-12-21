52ce6453910dd7ad8a15a217e85248ec320c0de7
youloveli <mymhj122@gmail.com>
2023-12-21 12:05:05 +0900

    :sparkles: 제목과 내용이 차있을때만 전송버튼이 눌리고 색이 바뀌게 해준다

--- app/src/main/java/com/chobo/onrest/PostWrite.kt
+++ app/src/main/java/com/chobo/onrest/PostWrite.kt
@@ -23,6 +23,10 @@ class PostWrite : AppCompatActivity() {
     private val maxCharacters1 = 30 // 최대 글자 수
     var maxToggleCount = 2 // 최대 토글 횟수
     var currentToggleCount = 0 // 현재 토글 횟수
+    var inputText1 = "" // 초기 값을 빈 문자열로 설정
+    var currentLength1 = 0
+    var inputText = "" // 초기 값을 빈 문자열로 설정
+    var currentLength = 0
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,7 +35,7 @@ class PostWrite : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
-        val toggleListener = CompoundButton.OnCheckedChangeListener { buttonView, isChecked ->
+     val toggleListener = CompoundButton.OnCheckedChangeListener { buttonView, isChecked ->
             if (isChecked) {
                 if (currentToggleCount >= maxToggleCount) {
                     // 최대 토글 횟수를 초과하면 다시 체크를 해제합니다.
@@ -61,13 +65,18 @@ class PostWrite : AppCompatActivity() {
 
         editText.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-                // 텍스트 변경 전에 호출되는 메서드
             }
-
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                // 첫 번째 EditText에 대한 코드 (maxCharacters = 300)
-                val inputText = s.toString()
-                val currentLength = inputText.length
+            }
+            override fun afterTextChanged(s: Editable?) {
+                inputText = s.toString()
+                currentLength = inputText.length
+                binding.send.isChecked = false
+                if (currentLength1 > 0){
+                    if (currentLength > 0){
+                        binding.send.isChecked = true
+                    }
+                }
                 captionTextView.text = "$currentLength/$maxCharacters" // 현재 글자 수를 보여줍니다
 
                 if (currentLength > maxCharacters) {
@@ -76,27 +85,27 @@ class PostWrite : AppCompatActivity() {
                     captionTextView.setTextColor(Color.parseColor("#E92626")) // 텍스트 색상을 변경합니다
                 } else {
                     // 글자 수 제한 안에 있을 때 텍스트 색상을 리셋합니다
-                    captionTextView1.setTextColor(Color.parseColor("#89857C"))
-                }
-            }
-
-            override fun afterTextChanged(s: Editable?) {
-                // 텍스트 변경 후에 호출되는 메서드
-            }
+                    captionTextView.setTextColor(Color.parseColor("#89857C"))
+                }            }
         })
-
         editText1 = binding.titleinput // EditText 참조 가져오기
         captionTextView1 = binding.titleTextnum // TextView 참조 가져오기
 
         editText1.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-                // 텍스트 변경 전에 호출되는 메서드
             }
-
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                // 두 번째 EditText에 대한 코드 (maxCharacters1 = 30)
-                val inputText1 = s.toString()
-                val currentLength1 = inputText1.length
+            }
+            override fun afterTextChanged(s: Editable?) {
+                // 텍스트 변경 후에 호출되는 메서드
+                inputText1 = s.toString()
+                currentLength1 = inputText1.length
+                binding.send.isChecked = false
+                if (currentLength1 > 0){
+                    if (currentLength > 0){
+                        binding.send.isChecked = true
+                    }
+                }
                 captionTextView1.text = "$currentLength1/$maxCharacters1" // 현재 글자 수를 보여줍니다
 
                 if (currentLength1 > maxCharacters1) {
@@ -108,16 +117,16 @@ class PostWrite : AppCompatActivity() {
                     captionTextView1.setTextColor(Color.parseColor("#89857C"))
                 }
             }
-
-            override fun afterTextChanged(s: Editable?) {
-                // 텍스트 변경 후에 호출되는 메서드
-            }
         })
-
         binding.send.setOnClickListener() {
-            startActivity(Intent(this, QuestList::class.java))
-            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
-            finish()
+            binding.send.isChecked = false
+            if (currentLength1 > 0){
+                if (currentLength > 0){
+                    startActivity(Intent(this, QuestList::class.java))
+                    overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+                    finish()
+                }
+            }
         }
         binding.gobackIcon.setOnClickListener() {
             super.onBackPressed()
