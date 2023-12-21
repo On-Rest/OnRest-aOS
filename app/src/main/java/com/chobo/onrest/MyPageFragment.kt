ddda4a8bc26405ab9e185aea27b520e34ab7d564
수잉 <djswpsrkaksskfskf@gmail.com>
2023-12-21 09:50:07 +0900

    :lipstick: :: xml의 이름 변경

--- app/src/main/java/com/chobo/onrest/MyPageFragment.kt
+++ app/src/main/java/com/chobo/onrest/MyPageFragment.kt
@@ -5,14 +5,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.chobo.onrest.databinding.FragmentPieChartBinding
+import com.chobo.onrest.databinding.FragmentMyPageBinding
 import com.github.mikephil.charting.animation.Easing
+import com.github.mikephil.charting.data.BubbleDataSet
+import com.github.mikephil.charting.data.LineDataSet
 import com.github.mikephil.charting.data.PieData
 import com.github.mikephil.charting.data.PieDataSet
 import com.github.mikephil.charting.data.PieEntry
 
 class MyPageFragment : Fragment() {
-    private var _binding: FragmentPieChartBinding? = null
+    private var _binding: FragmentMyPageBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -20,7 +22,7 @@ class MyPageFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View {
-        _binding = FragmentPieChartBinding.inflate(inflater, container, false)
+        _binding = FragmentMyPageBinding.inflate(inflater, container, false)
         return binding.root
     }
 
@@ -32,9 +34,9 @@ class MyPageFragment : Fragment() {
     private fun initPieChart() {
         // 데이터 추가는 Entry 클래스를 이용한다.
         val feelRatio = listOf(
-            PieEntry(33f),
-            PieEntry(33f),
-            PieEntry(33f),
+            PieEntry(40f),
+            PieEntry(21f),
+            PieEntry(39f),
         )
 
         val pieColors = listOf(
@@ -45,16 +47,13 @@ class MyPageFragment : Fragment() {
 
         val dataSet = PieDataSet(feelRatio, "")
 
+        dataSet.setSliceSpace(3f)
+
         dataSet.colors = pieColors
 
         dataSet.setDrawValues(false)
 
-        // legend.isEnabled : 범례 유무 설정
-        // isRotationEnabled : 차트 회전 활성화 여부 설정
-        // animateY(1200, Easing.EaseInOutCubic) : 애니메이션 시간, 효과 설정
         binding.feelPieChart.apply {
-
-
             data = PieData(dataSet)
 
             description.isEnabled = false
@@ -63,9 +62,13 @@ class MyPageFragment : Fragment() {
             holeRadius = 62f
             setTouchEnabled(false)
             animateY(1200, Easing.EaseInOutCubic)
-
-            // animate()를 호출하면 차트를 새로 고치기 위해 invalidate()를 호출할 필요가 없다.
             animate()
+        }
+    }
+    class PieDataSet {
+        var sliceSpace: Float = 0f
+        fun setSliceSpace() {
+            sliceSpace = 5f
         }
     }
 
@@ -73,5 +76,4 @@ class MyPageFragment : Fragment() {
         super.onDestroyView()
         _binding = null
     }
-
 }
