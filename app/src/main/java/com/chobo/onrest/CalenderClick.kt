e71b769b984f3254c5567c48f51f041c34caa6f9
youloveli <mymhj122@gmail.com>
2023-12-30 13:59:46 +0900

    :sparkles: CalenderClick의 감정 아이콘 선택한감정에따라서 바뀌게 설정

--- app/src/main/java/com/chobo/onrest/CalenderClick.kt
+++ app/src/main/java/com/chobo/onrest/CalenderClick.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.chobo.onrest.databinding.CalendarClickBinding
 import java.text.SimpleDateFormat
@@ -13,6 +14,12 @@ class CalenderClick : AppCompatActivity() {
     private lateinit var binding: CalendarClickBinding
     val date = Date() // 현재 날짜와 시간 가져오기
     val dayOfMonth = SimpleDateFormat("dd").format(date) // 일만 가져오기
+    val fileName = "missionData.txt" // 파일 이름r
+    var stringValue = "" // 저장할 데이터
+    val booleanValue = false
+    val angryImageDrawable = R.drawable.angry_face
+    val happyImageDrawable = R.drawable.happy_face
+    val sadImageDrawable = R.drawable.sad_face
 
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,13 +29,49 @@ class CalenderClick : AppCompatActivity() {
         setContentView(view)
         val sharedPrefs = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
         val retrievedValue = sharedPrefs.getString("memoinput", "defaultValue") // "key"에 해당하는 데이터를 가져옵니다. 만약 데이터가 없으면 기본값인 "defaultValue"가 반환됩니다.
+        val todaysEmotion = sharedPrefs.getString("yourEmotion", "defaultValue") // "key"에 해당하는 데이터를 가져옵니다. 만약 데이터가 없으면 기본값인 "defaultValue"가 반환됩니다.
         val selectedmission = intent.getStringExtra("key") // 받은 값
         val receivedList = intent.getSerializableExtra("myList") as? List<String>
 
+        when(todaysEmotion){
+            "angry" -> binding.todaysemotion.setImageResource(angryImageDrawable)
+            "happy" -> binding.todaysemotion.setImageResource(happyImageDrawable)
+            "sad" -> binding.todaysemotion.setImageResource(sadImageDrawable)
+            else ->  {
+                val intent =  Intent(this, EmotionChoice1::class.java)
+                startActivity(intent)
+                overridePendingTransition( android.R.anim.fade_in, android.R.anim.fade_out )
+                finish()
+            }
+        }
+
+        try {
+            val fileOutputStream = openFileOutput(fileName, Context.MODE_PRIVATE)
+
+            // boolean 값 파일에 쓰기
+            fileOutputStream.write(booleanValue.toString().toByteArray())
+            // 줄 바꿈을 추가하여 String 값 파일에 쓰기
+            fileOutputStream.write("\n".toByteArray())
+            fileOutputStream.write(stringValue.toByteArray())
+            fileOutputStream.write("\n".toByteArray())
+            fileOutputStream.close()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+
         when(selectedmission){
-            "1" ->  binding.checkTV.isChecked = true
-            "2" ->  binding.checkTV.isChecked = true
-            "3" ->  binding.checkTV.isChecked = true
+            "1" ->  {
+                stringValue = receivedList!![0]
+                binding.checkTV.isChecked = true
+            }
+            "2" ->  {
+                stringValue = receivedList!![1]
+                binding.checkTV.isChecked = true
+            }
+            "3" ->  {
+                stringValue = receivedList!![2]
+                binding.checkTV.isChecked = true
+            }
         }
 
         binding.memoinput.setText(retrievedValue)
