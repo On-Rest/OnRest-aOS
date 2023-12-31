490cde1db093a21ba64ba0cfcc2a931b083e32c4
youloveli <mymhj122@gmail.com>
2024-01-01 02:38:43 +0900

    :sparkles: 달력 클릭리스너 구현

--- app/src/main/java/com/chobo/onrest/CalendarFragment.kt
+++ app/src/main/java/com/chobo/onrest/CalendarFragment.kt
@@ -35,7 +35,7 @@ class CalendarFragment : Fragment() {
             startActivityWithAnimation(DiaryWrite::class.java)
         }
 
-        val days = listOf(binding.day1, binding.day2, binding.day3, binding.day4 /*, ... */)
+        val days = listOf(binding.day1, binding.day2, binding.day3, binding.day4 , binding.day5, binding.day6, binding.day7, binding.day8, binding.day9, binding.day10, binding.day11, binding.day12, binding.day13, binding.day14, binding.day15, binding.day16, binding.day18, binding.day19, binding.day20, binding.day21 , binding.day22 , binding.day23 , binding.day24, binding.day25, binding.day26, binding.day27, binding.day28 , binding.day29 , binding.day30 , binding.day31)
         for (day in days) {
             day.setOnClickListener(clickListener)
         }
