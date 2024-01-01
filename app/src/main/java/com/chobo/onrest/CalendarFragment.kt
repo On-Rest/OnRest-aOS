a3883309455ba9e37c6b95c1794eb03acc3185f9
youloveli <mymhj122@gmail.com>
2024-01-01 19:42:06 +0900

    :sparkles: 달력의 날마다의 감정이미지 바꾸기

--- app/src/main/java/com/chobo/onrest/CalendarFragment.kt
+++ app/src/main/java/com/chobo/onrest/CalendarFragment.kt
@@ -1,21 +1,26 @@
 package com.chobo.onrest
 
 import android.R
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import androidx.fragment.app.Fragment
 import com.chobo.onrest.databinding.CalendarBinding
+import java.io.BufferedReader
 import java.io.File
-import java.io.Serializable
+import java.io.FileNotFoundException
+import java.io.InputStreamReader
 
 class CalendarFragment : Fragment() {
-
     private lateinit var binding: CalendarBinding
-
+    val grayImageDrawable = com.chobo.onrest.R.drawable.gray_face
+    val fileLines = mutableListOf<String>()
+    var fileName = ""
+    var emotion = grayImageDrawable
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -47,17 +52,46 @@ class CalendarFragment : Fragment() {
         dayButtons.forEachIndexed { index, button ->
             val filesDir = requireContext().filesDir
             val date = String.format("%02d", index+1)
-            val myFile = File(filesDir, "2024-01-${date}")
-            if (myFile.exists()){
+            fileName = "2024-01-${date}"
+            val myFile = File(filesDir, fileName)
+            if (myFile.exists()) {
+                readFile(requireContext())
                 button.setOnClickListener {
                     val intent = Intent(requireContext(), CalenderClick1::class.java)
                     intent.putExtra("BUTTON_ID", date) // 데이터 전달
                     startActivity(intent)
                     requireActivity().overridePendingTransition(R.anim.fade_in, R.anim.fade_out)
                 }
+                emotion = when {
+                    fileLines.isEmpty() -> grayImageDrawable // 파일을 읽지 못한 경우
+                    fileLines[0] == "angry" -> com.chobo.onrest.R.drawable.angry_face
+                    fileLines[0] == "happy" -> com.chobo.onrest.R.drawable.happy_face
+                    fileLines[0] == "sad" -> com.chobo.onrest.R.drawable.sad_face
+                    else -> grayImageDrawable // 감정에 해당하는 이미지가 없을 경우 회색 이미지
+                }
+                button.setImageResource(emotion)
             }
         }
+    }
+    private fun readFile(context: Context){
+        fileLines.clear()
 
+        try {
+            val fileInputStream = context.openFileInput(fileName)
+            val inputStreamReader = InputStreamReader(fileInputStream)
+            val bufferedReader = BufferedReader(inputStreamReader)
+
+            val firstLine = bufferedReader.readLine() // 첫 번째 줄을 읽습니다.
+            if (firstLine != null) {
+                fileLines.add(firstLine) // 첫 번째 줄을 리스트에 추가합니다.
+            }
+
+            fileInputStream.close()
+        } catch (e: FileNotFoundException) {
+            Log.e("FileNotFound", "파일이나 디렉토리를 찾을 수 없습니다: $fileName")
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
     }
     private fun startActivityWithAnimation(clazz: Class<*>) {
         val intent = Intent(requireContext(), clazz)
