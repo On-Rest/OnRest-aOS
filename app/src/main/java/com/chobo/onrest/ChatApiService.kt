cd5682651059ea5b7bedee07d3109247c15a995c
youloveli <mymhj122@gmail.com>
2024-01-02 10:46:52 +0900

    :bug: 401에러 api key값 바꾸기

--- app/src/main/java/com/chobo/onrest/ChatApiService.kt
+++ app/src/main/java/com/chobo/onrest/ChatApiService.kt
@@ -6,7 +6,7 @@ import retrofit2.http.POST
 
 interface ChatApiService {
     @Headers("Content-Type: application/json",
-        "Authorization: Bearer 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")
+        "Authorization: Bearer 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")
     @POST("v1/chat/completions")
     suspend fun getChatCompletion(@Body requestBody: ChatRequestBody): ChatResponse
 }
