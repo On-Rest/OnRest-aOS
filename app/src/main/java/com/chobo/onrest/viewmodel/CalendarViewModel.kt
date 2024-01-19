4ee0cc2d3da693b58cecaf054aec9750ed1fbc1c
youloveli <mymhj122@gmail.com>
2024-01-19 21:28:43 +0900

    :white_check_mark: CalendarViewModel with문으로 간소화

--- app/src/main/java/com/chobo/onrest/viewmodel/CalendarViewModel.kt
+++ app/src/main/java/com/chobo/onrest/viewmodel/CalendarViewModel.kt
@@ -15,20 +15,44 @@ import java.util.Date
 
 @SuppressLint("SimpleDateFormat")
 class CalendarViewModel(application: Application, intent: Intent) : AndroidViewModel(application) {
-     val _calendarData = MutableLiveData<CalendarData>()
-    val calendarData: LiveData<CalendarData>
-        get() = _calendarData
     private val calendarModel: CalendarModel = CalendarModel(application)
     private val receivedList = intent.getSerializableExtra("myList") as? List<String> ?: emptyList()
     private val selectedMission = intent.getStringExtra("key") ?: ""
     private val sharedPrefs = application.getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
 
-    init {
-        _calendarData.value = CalendarData(
+    private fun writeToFileTodo(calendarData: CalendarData) {
+        with(calendarData) {
+            calendarModel.writefileTodo(
+                currentYear,
+                currentMonth,
+                todaysEmotion,
+                stringValue,
+                booleanValue
+            )
+        }
+    }
+
+    private fun writeToFile(calendarData: CalendarData) {
+        with(calendarData) {
+            calendarModel.writefile(
+                currentYear,
+                currentMonth,
+                currentDay,
+                todaysEmotion,
+                editedDate,
+                selectedMission,
+                receivedList,
+                retrievedValue
+            )
+        }
+    }
+
+    val calendarData: LiveData<CalendarData> = MutableLiveData<CalendarData>().apply {
+        value = CalendarData(
             currentDay = SimpleDateFormat("dd").format(Date()),
             currentYear = SimpleDateFormat("yyyy").format(Date()),
             currentMonth = SimpleDateFormat("MM").format(Date()),
-            selectedMission = intent.getStringExtra("key") ?: "",
+            selectedMission = selectedMission,
             stringValue = when (selectedMission) {
                 "1", "2", "3" -> receivedList.getOrNull(selectedMission.toInt() - 1) ?: ""
                 else -> ""
@@ -39,28 +63,9 @@ class CalendarViewModel(application: Application, intent: Intent) : AndroidViewM
             todaysEmotion = sharedPrefs.getString("yourEmotion", "defaultValue").toString(),
             receivedList = emptyList(),
         )
-        _calendarData.value?.let { calendarData ->
-            calendarModel.writefileTodo(
-                calendarData.currentYear,
-                calendarData.currentMonth,
-                calendarData.todaysEmotion,
-                calendarData.stringValue,
-                calendarData.booleanValue,
-            )
-        }
 
-        _calendarData.value?.let { calendarData ->
-            calendarModel.writefile(
-                calendarData.currentYear,
-                calendarData.currentMonth,
-                calendarData.currentDay,
-                calendarData.todaysEmotion,
-                calendarData.editedDate,
-                calendarData.selectedMission,
-                calendarData.receivedList,
-                calendarData.retrievedValue,
-            )
-        }
+        value?.let { writeToFileTodo(it) }
+
+        value?.let { writeToFile(it) }
     }
 }
-
